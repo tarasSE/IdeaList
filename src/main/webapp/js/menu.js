3fd68547b0d7f1c7bd2f916d859d31becd1542bd
taras <taras.serba@ukr.net>
2015-11-17 17:00:00 +0200

    Add webapp, Create navigation menu, content block , css , create logo.

---
+++ src/main/webapp/js/menu.js
@@ -0,0 +1,12 @@
+var main = function () {
+    $(document).ready(function () {
+        // Call horizontalNav on the navigations wrapping element
+        $('.full-width').horizontalNav({
+
+
+        });
+    });
+
+}
+
+$(document).ready(main);
